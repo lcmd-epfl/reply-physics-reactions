b5512f7c968b35851a3b6561dd5ffa98e2731647
puck <puck.vangerwen@gmail.com>
2023-06-08 15:46:15 +0200

    update figs

--- plot_performance.py
+++ plot_performance.py
@@ -3,6 +3,8 @@ import matplotlib
 import pandas as pd
 import numpy as np
 
+stuyver = True
+
 # read results
 cyclo_true_df = pd.read_csv('results/cyclo_true/test_scores.csv')
 cyclo_rxnmapper_df = pd.read_csv('results/cyclo_rxnmapper/test_scores.csv')
@@ -18,36 +20,47 @@ proparg_random_df = pd.read_csv('results/proparg/test_scores.csv')
 proparg_maes = np.load('data/proparg/slatm_10fold.npy')
 
 #matplotlib.rcParams["figure.figsize"] = (10, 4.4)
-matplotlib.rcParams.update({"font.size":14})
+matplotlib.rcParams.update({"font.size":13})
 
 fig, axes = plt.subplots(nrows=1, ncols=3)
-
-ax = axes[1]
-ax.set_title('Cyclo-23-TS')
 labels = ['CGR True', 'CGR RXNMapper', 'CGR Random', 'SLATM+KRR']
-for i, df in enumerate([cyclo_true_df, cyclo_rxnmapper_df, cyclo_random_df]):
-    ax.bar(i, df['Mean mae'], yerr=df['Standard deviation mae'])
-ax.bar(3, np.mean(cyclo_maes), yerr=np.std(cyclo_maes))
-ax.set_xticks(list(range(len(labels))))
-ax.set_xticklabels(labels, rotation=90)
 
 ax = axes[0]
 ax.set_ylabel("MAE $\Delta G^\ddag$ [kcal/mol]")
-ax.set_title('GDB7-22-TS')
+ax.set_title('(a) GDB7-22-TS')
 for i, df in enumerate([gdb_true_df, gdb_rxnmapper_df, gdb_random_df]):
     ax.bar(i, df['Mean mae'], yerr=df['Standard deviation mae'])
 ax.bar(3, np.mean(gdb_maes), yerr=np.std(gdb_maes))
-ax.set_xticks(list(range(len(labels))))
-ax.set_xticklabels(labels, rotation=90)
+ax.set_xticks([])
+
+ax = axes[1]
+ax.set_title('(b) Cyclo-23-TS')
+for i, df in enumerate([cyclo_true_df, cyclo_rxnmapper_df, cyclo_random_df]):
+    ax.bar(i, df['Mean mae'], yerr=df['Standard deviation mae'], label=labels[i])
+ax.bar(3, np.mean(cyclo_maes), yerr=np.std(cyclo_maes), label=labels[3])
+
+if stuyver:
+    ax.bar(4, 2.96, label='QM-GNN True')
+    ax.bar(5, 3.09, label='WLN-GNN True')
+ax.set_xticks([])
 
 ax = axes[2]
-ax.set_title("Proparg-21-TS")
-ax.bar(0, proparg_random_df['Mean mae'], yerr=df['Standard deviation mae'], color='C2')
-ax.bar(1, np.mean(proparg_maes), yerr=np.std(proparg_maes), color='C3')
-ax.set_xticks([0,1])
-ax.set_xticklabels(['CGR Random', 'SLATM+KRR'], rotation=90)
+ax.set_title("(c) Proparg-21-TS")
+for i in range(2):
+    ax.bar(i, 0)
+ax.bar(2, proparg_random_df['Mean mae'], yerr=df['Standard deviation mae'])
+ax.bar(3, np.mean(proparg_maes), yerr=np.std(proparg_maes))
+ax.set_xticks([])
 
-figname = 'figures/atom_mapping_quality.pdf'
-
+handles, labels = axes[1].get_legend_handles_labels()
 plt.tight_layout()
-plt.savefig(figname)
+
+plt.legend(handles, labels, bbox_to_anchor=(1.01,1.01))
+
+if not stuyver:
+    figname = 'figures/atom_mapping_quality.pdf'
+else:
+    figname = 'figures/atom_mapping_quality_stuyver.pdf'
+
+plt.savefig(figname, bbox_inches='tight')
+#plt.show()
